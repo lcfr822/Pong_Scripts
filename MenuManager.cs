e56128de9b81fc945994c7ddf072790e40ceb7c5
lcfr822 <lcfr822@gmail.com>
2018-04-20 14:28:49 -0700

    Code Cleaned
    Organized my code

--- MenuManager.cs
+++ MenuManager.cs
@@ -13,26 +13,14 @@ public class MenuManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        if(Application.platform == RuntimePlatform.WebGLPlayer)
-        {
-            Destroy(qButton.gameObject);
-        }
+        if (Application.platform == RuntimePlatform.WebGLPlayer) { Destroy(qButton.gameObject); }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Jump") && !Input.GetButton("Fire1"))
-        {
-            StartCoroutine(StartGame());
-        }
-        else if (Input.GetButtonDown("Fire1"))
-        {
-            controlGroup.alpha = 1;
-        }
-        else if (Input.GetButtonUp("Fire1"))
-        {
-            controlGroup.alpha = 0;
-        }
+        if (Input.GetButtonDown("Jump") && !Input.GetButton("Fire1")) { StartCoroutine(StartGame()); }
+        else if (Input.GetButtonDown("Fire1")) { controlGroup.alpha = 1; }
+        else if (Input.GetButtonUp("Fire1")) { controlGroup.alpha = 0; }
 	}
 
     public void Quit()
